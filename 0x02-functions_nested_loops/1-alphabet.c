a0dc4f017730a1f4e74bcac4b95f7228883c9151
Hawi <hawigirmachee@gmail.com>
2022-04-12 12:34:23 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * to print lowercase alphabets
+ * Return: return type void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
