e03168372bb2693eeb5199b6ae74d9889a411cb7
Hawi <hawigirmachee@gmail.com>
2022-04-12 12:05:53 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**main- entry block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char hggs[] = "_putchar";
+	int c;
+
+	for(c=0; c < 8; c++)
+	{
+		_putchar(hggs[c]);
+	}
+	_putchar("\n");
+	return (0);
+}
