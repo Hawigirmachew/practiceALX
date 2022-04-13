822476ab24530f0bb20f47e84b1af3a1df7a9467
Hawi <hawigirmachee@gmail.com>
2022-04-13 05:52:40 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,15 +22,14 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k);
+				_putchar(k + '0');
 			}
 			else if (k >= 0)
 			{
 				_putchar(' ');
 				_putchar(',');
-				_putchar(' ');
-				_putchar(k / 10);
-				_putchar(k % 10);
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
