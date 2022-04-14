953480cc200130621a0b159c20882ed5d4c227a3
Hawi <hawigirmachee@gmail.com>
2022-04-14 08:16:34 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Returns: Always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+	_putchar('\n');
+}
