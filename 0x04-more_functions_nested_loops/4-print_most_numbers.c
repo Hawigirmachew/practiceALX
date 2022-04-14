57af3bdf8a8309cf4ed7802546a0a96bda28ac69
Hawi <hawigirmachee@gmail.com>
2022-04-14 08:25:10 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.c"
+/**
+ * print_most_numbers - print numbers without 4 and 2
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 && i == 4)
+			break;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
