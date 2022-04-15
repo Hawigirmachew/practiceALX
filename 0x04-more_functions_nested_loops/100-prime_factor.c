75aac40394f6e13a04ad7fd750254c36f7185b83
Hawi <hawigirmachee@gmail.com>
2022-04-15 09:54:55 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry block
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int n, i, max;
+
+	n = 612852475143;
+	max = 1;
+
+	for (i = 3; i <= n; i+=2)
+	{
+		while (n % i == 0)
+		{
+			max = n;
+			n = n / i;
+		}
+	}
+	printf("%lu\n", max);
+	return 0;
+}
