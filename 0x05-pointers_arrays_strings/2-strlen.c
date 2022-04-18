61bee76e4dae88c05b5f0e72670934d68dfd47c2
Hawi <hawigirmachee@gmail.com>
2022-04-18 10:53:04 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - calculates and returns length of char
+ * @s: A pointer points to character
+ * Returns: the length of the char
+ */
+int _strlen(char *c)
+{
+	int len;
+
+	len = strlen(c);
+	return(len);
+}
