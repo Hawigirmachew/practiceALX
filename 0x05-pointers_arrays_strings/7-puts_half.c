312e2b4e22793188e1ed5597aedfe131df31ba97
Hawi <hawigirmachee@gmail.com>
2022-04-18 19:06:57 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints the  half of given 
+ * @str: pointer points to char
+ *
+ * Return: void type
+ */
+void puts_half(char *str)
+{
+	int n, last;
+
+	while (str[n] != '\n')
+	{
+		n++;
+	}
+
+	last = n - 1 / 2;
+
+	for (n = last; str[n] != '\n'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
+
+
