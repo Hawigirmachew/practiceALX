fbdd7cef3cb938ddc91c9eb6171d1f83b3b5910b
Hawi <hawigirmachee@gmail.com>
2022-04-18 18:52:54 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints half of the entered number
+ * @str: a pointer points to character
+ * Returns: Always 0
+ */
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\n'; n++)
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+		_putchar('\n');
+	}
+}
