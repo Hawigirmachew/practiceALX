46939cf28e14e52cbf53f25bc70b82ab09611277
Hawi <hawigirmachee@gmail.com>
2022-04-21 05:37:07 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,11 @@ char *string_toupper(char *s)
 {
 	while (*s != '\0')
 	{
-		if (*s >= 65 && *s <= 90)
+		if (*s >= 97 && *s <= 122)
 		{
-			*s = *s + 32;
+			*s = *s - 32;
 		}
 		s++;
 	}
-	printf("%c", *s);
+	return (s);
 }
