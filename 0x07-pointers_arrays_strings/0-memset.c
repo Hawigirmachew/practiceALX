17d1987ed2278ae9454994752e60bd936b80c762
Hawi <hawigirmachee@gmail.com>
2022-04-25 10:44:35 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,15 +3,18 @@
 * _memset - to change array element
 * @s: pointer points to array of char
 * @b: takes char
-* @size: takes int
+* @size: takes unsigned int
 * Return: char value
 */
 char *_memset(char *s, char b, unsigned int size)
 {
+
+int n = size;
+
+if (size > 0){
 int i;
 
 for (i = 0; i < size; i++)
-{
 s[i] = b;
 }
 return (s);
