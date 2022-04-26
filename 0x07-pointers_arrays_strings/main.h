38fb57ac082139ddf794534acbe6e55ad12ec88c
Hawi <hawigirmachee@gmail.com>
2022-04-26 04:58:21 -0700

    Main File

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+char *_strpbrk(char *s, char *accept);
 #endif
